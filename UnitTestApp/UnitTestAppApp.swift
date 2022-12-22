2b33e69e8b202ecb9e427dfd522392f125b73acf
Majkel <majkelkrol@icloud.com>
2022-12-22 21:43:46 +0100

    Initial commit

---
+++ UnitTestApp/UnitTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitTestAppApp.swift
+//  UnitTestApp
+//
+//  Created by Majkel on 22/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct UnitTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
